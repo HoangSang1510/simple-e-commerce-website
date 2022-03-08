320691e86381c1ea39106fda6e45b832b9eae8dc
HoangSang1510 <59404467+HoangSang1510@users.noreply.github.com>
2022-03-08 16:01:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -116,11 +116,13 @@ app.use((req, res, next)=>{
 route(app)
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
 
-
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 
